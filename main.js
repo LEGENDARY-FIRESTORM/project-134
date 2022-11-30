5eacca44672e4071e96be41d084a4a17599bf837
LEGENDARY-FIRESTORM <94469035+LEGENDARY-FIRESTORM@users.noreply.github.com>
2022-11-30 17:51:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+status = "";
+objects = [];
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - detecting objects";
+}
+
+function preload(){
+    alarm = loadSound("mixkit-critical-alarm-1004.wav");
+}
+
+function draw(){
+    image(video, 0, 0, 640, 420);
+
+    if(status != ""){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        for(i = 0; i < objects.length; i++){
+            object_name = objects[i].label;
+            document.getElementById("status").innerHTML = "Status - detected objects";
+            percent = floor(objects[i].confidence * 100);
+            fill(r, g, b);
+            noFill();
+            stroke(r, g, b);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y + 10);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(object_name == "person"){
+                alarm.stop();
+                document.getElementById("number").innerHTML = "Baby Found";
+            }
+            
+            else{
+                alarm.play();
+                document.getElementById("number").innerHTML = "Baby not Found";
+            }
+        }
+
+        if(objects.length == 0){
+            alarm.play();
+            document.getElementById("number").innerHTML = "Baby not Found";
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+    status = true;
+    object_detector.detect(video, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
